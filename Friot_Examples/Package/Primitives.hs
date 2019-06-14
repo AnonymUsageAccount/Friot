b704dc7f882a5a4b89e36e2d21b2e1feb4ee904d
anonym <2550219961@qq.com>
2019-06-14 17:26:34 +0800

    add the respeberry lib

--- Friot_Examples/Package/Primitives.hs
+++ Friot_Examples/Package/Primitives.hs
@@ -1,11 +1,35 @@
 module Package.Primitives where
     
+-- REAVTIVE FRAMEWORK, CLINE SERVER COMMUNICATION
 reactIoT:: a -> (a -> b  -> a) -> (a -> c b) -> IO ()
 reactIoT init update view = print 1
 
-staticIoT::  (a -> c b) -> IO ()
-staticIoT  view = print 1
 
+-- STATIC CONTROL, NO COMMUNICATION
+staticIoT::  (a -> c b) -> IO ()
+staticIoT  view = print 2
 
 static2IoT::a ->  (a -> c b) -> IO ()
-static2IoT init view = print 1
+static2IoT init view = print 3
+
+
+-- REAVTIVE FRAMEWORK, CLINE SERVER COMMUNICATION AND SUBSCRIPTION 
+subReactIoT:: [d] -> a -> (a -> b  -> a) -> (a -> c b) -> IO ()
+subReactIoT sub init update view = print 4
+
+-- SUBSCRIPTION FRAMEWORK,  
+subscripIoT:: [d] ->  IO ()
+subscripIoT sub = print 5
+
+getMotion :: Bool
+getMotion  = False
+
+getTemp :: Int
+getTemp = 1
+
+type Event = IO ()
+
+ev :: String -> Event 
+ev inp = print 1
+
+
